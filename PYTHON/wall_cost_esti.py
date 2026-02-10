0d8fe020f63bb34dc5e944db5da00b520a55efca
kishore <kishorelang@gmail.com>
2026-02-10 22:14:39 +0530

    Delete PYTHON directory

--- PYTHON/wall_cost_esti.py
+++
@@ -1,7 +0,0 @@
-import math
-def get_cost(sqft_walls, sqft_ceiling, sqft_per_gallon, cost_per_gallon):
-    cost = math.ceil(((sqft_walls+sqft_ceiling)/sqft_per_gallon )) * cost_per_gallon
-    return cost
-
-project_cost = get_cost(432,144,400,15) 
-print(project_cost)
