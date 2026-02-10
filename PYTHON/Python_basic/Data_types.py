334910a050cba8ac1d7860db9c4958aa5a438519
kishore <kishorelang@gmail.com>
2026-02-10 22:18:59 +0530

    Add files via upload

---
+++ PYTHON/Python_basic/Data_types.py
@@ -0,0 +1,22 @@
+#identifying
+a= 1
+b=122.22
+c= "p"
+d= "kishore"
+e = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+f=1.
+g=2<10
+h= 3>11
+print(f)
+print(g)
+print(h)
+
+k = not h
+
+print(k)
---
+++ PYTHON/Python_basic/create_var.py
@@ -0,0 +1,10 @@
+a =2
+b = 2
+c = a+b
+print(c)
+
+#manupulation 
+my_var=3
+print(my_var)
+my_var = 100
+print(my_var)
