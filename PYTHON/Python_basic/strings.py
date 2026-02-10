c10a584b27712b8b7679b34cd08bf82ab52bf0cc
kishore <kishorelang@gmail.com>
2026-02-10 22:16:08 +0530

    Add files via upload

---
+++ PYTHON/Python_basic/strings.py
@@ -0,0 +1,21 @@
+a=""
+print(a)
+print(type(a))
+print(len(a))
+
+#concate
+b="abc" + "abc"
+print(b)
+
+#multiply
+c = "ab"*5
+print(c)
+
+#conversion
+number = "1.2233"
+print(number)
+print(type(number))
+
+converted = float(number)
+print(converted)
+print(type(converted))
